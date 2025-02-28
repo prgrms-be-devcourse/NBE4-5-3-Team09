d1b7d84eb1abea7189abadcbd33678c6c66ea905
Jang Muyeong <136911104+wkdan@users.noreply.github.com>
2025-02-28 14:21:39 +0900

    feat: init project (#7)
    * feat: init project
    
    * refactor: convert configuration file from properties to YAML
    
    * chore: update h2 dependency scope from runtimeOnly to testRuntimeOnly

---
+++ backend/src/main/java/com/coing/CoingApplication.java
@@ -0,0 +1,13 @@
+package com.coing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/coing/CoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
