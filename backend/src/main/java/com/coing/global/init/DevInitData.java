88dac0792c6f230757a3046c111052aede7c0b95
이승민 <101695929+min429@users.noreply.github.com>
2025-03-06 10:52:44 +0900

    feat: add dev profile (#51)

--- backend/src/main/java/com/coing/global/init/DevInitData.java
+++ backend/src/main/java/com/coing/global/init/DevInitData.java
@@ -16,9 +16,11 @@ import java.util.List;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Profile("dev")
 @Slf4j
 @Configuration
 public class DevInitData {
