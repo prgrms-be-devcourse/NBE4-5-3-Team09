73533ec36b649b9e1eb7a3771aa6111a8488dffc
김하연 <vthyrachel@naver.com>
2025-03-11 22:12:01 +0900

    Fix/100 fix market card ui (#108)

--- frontend/src/app/coin/list/MarketList.tsx
+++ frontend/src/app/coin/list/MarketList.tsx
@@ -34,66 +34,69 @@ export default function ClientPage({ markets }: ClientPageProps) {
   }, [marketCodes, updateSubscriptions]);
 
   return (
-      <div className="grid grid-cols-3 gap-4 px-2 mt-5">
-        {markets.map((market) => {
-          const ticker = tickers[market.code];
-          return (
-              <Link key={market.code} href={`/coin/${market.code}`}>
-                <Card className="bg-white shadow-sm rounded-sm border-0">
-                  <CardContent className="p-4">
-                    <div className="flex items-center space-x-1">
-                      <h2 className="text-base font-bold">{market.koreanName}</h2>
-                      <h3 className="text-sm text-gray-500">{market.englishName}</h3>
-                    </div>
-                    <div className="flex justify-between items-end mt-1">
-                      <p className="text-xl font-semibold">
-                        {ticker
-                            ? new Intl.NumberFormat(undefined, {
-                              minimumFractionDigits:
-                                  ticker.tradePrice <= 1
-                                      ? 8
-                                      : ticker.tradePrice < 1000
-                                          ? 1
-                                          : 0,
-                              maximumFractionDigits:
-                                  ticker.tradePrice <= 1
-                                      ? 8
-                                      : ticker.tradePrice < 1000
-                                          ? 1
-                                          : 0,
-                            }).format(ticker.tradePrice)
-                            : "0"}{" "}
-                        <span className="text-xs">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-2 mt-5">
+      {markets.map((market) => {
+        const ticker = tickers[market.code];
+        return (
+          <Link key={market.code} href={`/coin/${market.code}`}>
+            <Card className="bg-white shadow-sm rounded-sm border-0">
+              <CardContent className="p-4">
+                <div className="flex items-center space-x-1">
+                  <h2 className="text-base font-bold truncate">{market.koreanName}</h2>
+                  <h3 className="text-sm text-gray-500 truncate">{market.englishName}</h3>
+                </div>
+                <div className="flex justify-between items-end mt-1">
+                  <p className="text-xl font-semibold">
+                    {ticker
+                      ? new Intl.NumberFormat(undefined, {
+                        minimumFractionDigits:
+                          ticker.tradePrice <= 1
+                            ? 8
+                            : ticker.tradePrice < 1000
+                              ? 1
+                              : 0,
+                        maximumFractionDigits:
+                          ticker.tradePrice <= 1
+                            ? 8
+                            : ticker.tradePrice < 1000
+                              ? 1
+                              : 0,
+                      }).format(ticker.tradePrice)
+                      : "0"}{" "}
+                    <span className="text-xs">
                       {market.code.split("-")[0]}
                     </span>
-                      </p>
-                      <p
-                          className={
-                            ticker && ticker.signedChangeRate >= 0
-                                ? "text-sm text-red-500"
-                                : "text-sm text-blue-500"
-                          }
-                      >
-                        {ticker && ticker.signedChangeRate
-                            ? `${ticker.signedChangeRate >= 0 ? "+" : ""}${(
-                                ticker.signedChangeRate * 100
-                            ).toFixed(2)}%`
-                            : "0%"}
-                      </p>
-                    </div>
-                    <p className="text-xs text-gray-500 mt-2">
-                      거래량:{" "}
-                      {ticker
-                          ? new Intl.NumberFormat().format(
-                              parseFloat(ticker.accTradeVolume?.toFixed(3) || "0")
-                          )
-                          : "0"}
-                    </p>
-                  </CardContent>
-                </Card>
-              </Link>
-          );
-        })}
-      </div>
+                  </p>
+                  <p
+                    className={
+                      ticker && ticker.signedChangeRate >= 0
+                        ? "text-sm text-red-500"
+                        : "text-sm text-blue-500"
+                    }
+                  >
+                    {ticker && ticker.signedChangeRate
+                      ? `${ticker.signedChangeRate >= 0 ? "+" : ""}${(
+                        ticker.signedChangeRate * 100
+                      ).toFixed(2)}%`
+                      : "0%"}
+                  </p>
+                </div>
+                <p className="text-xs text-gray-500 mt-2">
+                  거래량{" "}
+                  {ticker
+                    ? new Intl.NumberFormat().format(
+                      parseFloat(ticker.accTradeVolume?.toFixed(3) || "0")
+                    )
+                    : "0"}{" "}
+                  <span className="text-xs">
+                    {market.code.split("-")[1]}
+                  </span>
+                </p>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
   );
 }
