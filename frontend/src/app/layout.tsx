e93548582212fddd0bd49d70614a8fff892bbe39
2hwayoung <69628269+2hwayoung@users.noreply.github.com>
2025-03-13 21:00:33 +0900

    feat: apply font (#139)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '@/app/globals.css';
+import './globals.css';
 import { AuthProvider } from '@/context/AuthContext';
 import Header from '@/components/Header';
 import { ThemeProvider } from '@/context/ThemeContext';
