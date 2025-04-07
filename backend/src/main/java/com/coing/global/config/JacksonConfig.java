81ba5cf9599870ef1e6be0b7967f3587e8352af9
min429 <lsm990429@gmail.com>
2025-04-07 09:41:32 +0900

    chore: add kotlinModule to custom ObjectMapper

--- backend/src/main/java/com/coing/global/config/JacksonConfig.java
+++ backend/src/main/java/com/coing/global/config/JacksonConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
 
 @Configuration
 public class JacksonConfig {
 	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper()
+			.registerModule(new KotlinModule.Builder().build())
 			.registerModule(new JavaTimeModule()) // Java 8 날짜/시간 지원 추가
 			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // timestamp 변환 방지
 	}
